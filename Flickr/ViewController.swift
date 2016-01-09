123407e3392c734f575a1b9e42e3dd99788f78ea
kioko <kiokotomas@gmail.com>
2016-01-09 12:32:29 +0300

    Added outlet and action reference

--- Flickr/ViewController.swift
+++ Flickr/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoTitle: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func touchGrabNewImageButton(sender: AnyObject) {
+       
+    }
 
 }
 
